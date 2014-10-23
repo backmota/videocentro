1edcbb4ebb3bb9ee84ec77a5a800cd416c907299
mdxx66 <jj.mota.t@me.com>
2014-10-23 17:19:36 -0500

    View.py
    Actor

--- videocentro/actor/views.py
+++ videocentro/actor/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import *
+from .forms import *
+from django.contrib.auth import *
+from django.contrib.auth.decorators import *
+from django.contrib.auth import login
 # Create your views here.
+
+def signup(request):
+	form = UserCreationEmailForm(request.POST or None)
+
+	if form.is_valid():
+		form.save()
+
+	return render(request, 'signup.html',{'form':form})	
